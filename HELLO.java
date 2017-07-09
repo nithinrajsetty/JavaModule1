8287a5a3406457096d0af3ad40806a12ac951da5
nithinrajsetty <nithinrajsetty4@gmail.com>
2017-07-09 12:53:04 +0530

    Add files via upload

---
+++ HELLO.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1;
+	  for(x1=1;x1<5;x1++)
+	  System.out.println("HELLO");
+	 
+	}
+}
