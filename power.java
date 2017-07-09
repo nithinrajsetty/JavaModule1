d46f55d44ab4bb51be4d8de35f34098befd3d713
nithinrajsetty <nithinrajsetty4@gmail.com>
2017-07-09 15:45:33 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1,x2,i;
+	  Scanner s= new Scanner(System.in);
+	  x1=s.nextInt();
+	  x2=s.nextInt();
+	  for(i=0;i<=x2;x2++);
+	  {
+	   x1=x1*x1;
+	   System.out.println("result:"+x1);
+	  }
+	  }
+}
