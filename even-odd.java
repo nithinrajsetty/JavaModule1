5d907f8951acf28ae0cfa57fb38f1508a5f1dfd9
nithinrajsetty <nithinrajsetty4@gmail.com>
2017-07-09 12:24:46 +0530

    Add files via upload

---
+++ even-odd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1;
+	  Scanner s= new Scanner(System.in);
+	  System.out.println("ENTER x1:");
+	  x1=s.nextInt();
+	  if(x1%2==0)
+	  System.out.println("x1 is even");
+          else 
+	  System.out.println("x1 is odd");
+	  
+	}
+}
