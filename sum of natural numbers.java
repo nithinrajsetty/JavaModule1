37036e9d4e5bbfab03155146536f437c6605631b
nithinrajsetty <nithinrajsetty4@gmail.com>
2017-07-09 14:20:29 +0530

    Add files via upload

---
+++ sum of natural numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x1,i;
+	  int s1=0;
+	 Scanner s= new Scanner(System.in);
+	 x1=s.nextInt();
+	 for(i=1;i<x1;i++)
+	 s1=s1+i;
+	 System.out.println("sum:"+s1);
+  	}
+}
