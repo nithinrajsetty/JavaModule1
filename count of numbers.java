285bdd434481ab44791a7f585e23d4bd7f4ba703
nithinrajsetty <nithinrajsetty4@gmail.com>
2017-07-09 14:40:13 +0530

    Add files via upload

---
+++ count of numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n;
+	  int c=0;
+	 Scanner s= new Scanner(System.in);
+	 n=s.nextInt();
+	 while(n>0)
+	 {
+	   n=n/10;
+	   c++;
+	 }
+	 System.out.println("count:"+c);
+  	}
+}
